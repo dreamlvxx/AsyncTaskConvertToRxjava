b2683658baf1c55277184a337282d453bc0b4dcb
LVXINGXING <18734825542@163.com>
2020-12-24 17:39:50 +0800

    test amend

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("");
 
 //        AtomicInteger integer = new AtomicInteger();
 //        CoroutinePro<String,String,String> co = new CoroutinePro<String,String,String>(){
