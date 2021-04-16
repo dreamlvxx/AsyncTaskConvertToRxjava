e982c93872cbaa22926fa34e60dff6e127da54e6
LVXINGXING <18734825542@163.com>
2021-04-16 16:55:49 +0800

    添加rxjava log

--- app/src/main/java/com/example/myapplication/Rxjava.java
+++ app/src/main/java/com/example/myapplication/Rxjava.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.util.Log;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -7,6 +9,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public final class Rxjava {
     public static <T> Observable<T> defaultO(ObservableOnSubscribe<T> observable){
+        Log.e("xxx", "defaultO: Rxjava");
         return Observable
                 .create(observable).observeOn(AndroidSchedulers.mainThread());
     }
